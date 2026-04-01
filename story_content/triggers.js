cd4f6246b3ae365e634e8a1bd4142b4885a33fc6
kiet-design <kiet@bc.edu>
2026-03-31 21:09:43 -0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,46 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5w5WiMVJHXp":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6aJ2RpxP6rW');
+const duration = 750;
+const easing = 'cubic-bezier(0.25, 1, 0.5, 1)';
+const id = '6aIZ4BH3vje';
+const growAmount = 1.2;
+player.addForTriggers(
+id,
+target.animate(
+player.emphasis.elastic(growAmount)
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
